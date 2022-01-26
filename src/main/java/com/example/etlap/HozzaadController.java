acd1b9aeaddd0b18f72c64ffacfcd171caae83b6
Szalai Szabolcs <sz.szalai16@gmail.com>
2022-01-26 15:31:44 +0100

    hozzaad view

---
+++ src/main/java/com/example/etlap/HozzaadController.java
@@ -0,0 +1,23 @@
+package com.example.etlap;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class HozzaadController {
+    @javafx.fxml.FXML
+    private TextArea labelHozzadLeiras;
+    @javafx.fxml.FXML
+    private MenuButton labelHozzadKategoria;
+    @javafx.fxml.FXML
+    private Spinner labelHozzadAr;
+    @javafx.fxml.FXML
+    private TextField labelHozzadId;
+
+    @javafx.fxml.FXML
+    public void btnHozzaadClick(ActionEvent actionEvent) {
+
+    }
+}
