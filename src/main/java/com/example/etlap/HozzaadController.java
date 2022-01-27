7acc318c0bf0fec4882929d986d9301e298bf8b1
Szalai Szabolcs <sz.szalai16@gmail.com>
2022-01-27 12:13:02 +0100

    új étel felvétele

--- src/main/java/com/example/etlap/HozzaadController.java
+++ src/main/java/com/example/etlap/HozzaadController.java
@@ -1,30 +1,49 @@
 package com.example.etlap;
 
 import javafx.event.ActionEvent;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.Spinner;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HozzaadController {
     @javafx.fxml.FXML
     private TextField labelHozzadNev;
     @javafx.fxml.FXML
-    private TextArea labelHozzadLeiras;
+    private TextArea textAreaHozzadLeiras;
     @javafx.fxml.FXML
-    private MenuButton labelHozzadKategoria;
+    private ComboBox<String> comboBoxHozzaadKategoria;
     @javafx.fxml.FXML
-    private Spinner<Integer> labelHozzadAr;
+    private Spinner<Integer> spinnerHozzadAr;
+    private EtelDB db;
+
+    public void initialize() {
+        List<String> kateg = new ArrayList();
+        try {
+            db = new EtelDB();
+            List<Etel> etelek = db.getEtelek();
+            comboBoxHozzaadKategoria.getItems().clear();
+            for (Etel etel : etelek) {
+                if (!kateg.contains(etel.getKategoria())) kateg.add(etel.getKategoria());
+            }
+            for (String kategoria: kateg) {
+                comboBoxHozzaadKategoria.getItems().add(kategoria);
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }
+    }
 
     @javafx.fxml.FXML
     public void btnHozzaadClick(ActionEvent actionEvent) {
         String nev = labelHozzadNev.getText();
-        String leiras = labelHozzadLeiras.getText();
-        String kategoria = labelHozzadKategoria.getText();
-        int ar = labelHozzadAr.getValue();
-
+        String leiras = textAreaHozzadLeiras.getText();
+        String kategoria = comboBoxHozzaadKategoria.getSelectionModel().getSelectedItem();
+        int ar = spinnerHozzadAr.getValue();
+        // TODO: 2022. 01. 27. alertek 
         try {
-            EtelDB db = new EtelDB();
+            db = new EtelDB();
             db.etelHozzadasa(nev, leiras, ar, kategoria);
         } catch (Exception e) {
             System.out.println(e);
